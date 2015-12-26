660ea63c598186f3c9fe05959ce293170fd158b0
thoniorf <thoniorf@gmail.com>
2015-12-26 20:49:55 +0100

    Update query.php
    Replace in-code db access credentials with env-vars

--- web/query.php
+++ web/query.php
@@ -1,19 +1,30 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+$host = getenv('host_db');
+$user = getenv('user_db');
+$pswd = getenv('pswd_db');
 
-$conn = new mysqli("127.0.0.1", "moderator", "1234", "Ticketsys_db");
+// Create connection
+//$conn = new mysqli("localhost", "moderator", "1234");
+$conn = new mysqli($host,$user,$pswd);
 
-$result = $conn->query("SELECT * FROM User");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+print"Connected successfully\n";
+$result = $conn->query("SELECT * FROM ticketsys_db.User");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
     $outp .= '{"id":"'  . $rs["idUser"] . '",';
-    $outp .= '"Email":"'   . $rs["Email"]        . '",'; 
+    $outp .= '"Email":"'   . $rs["Email"]        . '"}'; 
 }
 $outp ='{"records":['.$outp.']}';
 $conn->close();
 
-echo($outp);
+print($outp);
 ?>
+
