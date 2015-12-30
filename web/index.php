e244b9ef1f92744d26607ec5258fa23b41d37a61
thoniorf <thoniorf@gmail.com>
2015-12-30 19:30:11 +0100

    Added print placeholder
    Updated index.php
    Updated topbar.hp

--- web/index.php
+++ web/index.php
@@ -1,14 +1,15 @@
 <?php
-	require_once( $INCLUDE_PATH . "/config.php" );
-	require_once( $TEMPLATES_PATH . "/header.php" );
+	require_once $INCLUDE_PATH . '/config.php';
+	require_once $TEMPLATES_PATH . '/header.php';
 ?>
 <body>
 	<div class="container">
-		<?php 
-			require_once($CONTENTS_PATH . $current_content);
+		<?php
+			require_once $TEMPLATES_PATH . '/topbar.php';
+			require_once $CONTENTS_PATH . $current_content;
 		?>
 	</div>
 </body>
 <?php 
-	require_once ($TEMPLATES_PATH . "/footer.php");
+	require_once $TEMPLATES_PATH . '/footer.php';
 ?>
