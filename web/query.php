7b26f1770342d966aa1344c8ce24d81eda7089c3
thoniorf <thoniorf@gmail.com>
2016-01-02 18:03:02 +0100

    Updated sample page query.php

--- web/query.php
+++ web/query.php
@@ -1,29 +1,31 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-$host = getenv('host_db');
-$user = getenv('user_db');
-$pswd = getenv('pswd_db');
+header ( "Access-Control-Allow-Origin: *" );
+header ( "Content-Type: application/json; charset=UTF-8" );
+$host = getenv ( 'host_db' );
+$user = getenv ( 'user_db' );
+$pswd = getenv ( 'pswd_db' );
 
 // Create connection
-$conn = new mysqli($host,$user,$pswd);
+$conn = new mysqli ( $host, $user, $pswd );
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	die ( "Connection failed: " . $conn->connect_error );
 }
-print"Connected successfully\n";
-$result = $conn->query("SELECT * FROM ticketsys_db.User");
+print "Connected successfully\n";
+$result = $conn->query ( "SELECT * FROM ticketsys_db.User" );
 
 $outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"id":"'  . $rs["idUser"] . '",';
-    $outp .= '"Email":"'   . $rs["Email"]        . '"}'; 
+while ( $rs = $result->fetch_array ( MYSQLI_ASSOC ) ) {
+	if ($outp != "") {
+		$outp .= ",";
+	}
+	$outp .= '{"id":"' . $rs ["idUser"] . '",';
+	$outp .= '"Email":"' . $rs ["Email"] . '"}';
 }
-$outp ='{"records":['.$outp.']}';
-$conn->close();
+$outp = '{"records":[' . $outp . ']}';
+$conn->close ();
 
-print($outp);
+print ($outp) ;
 ?>
 
