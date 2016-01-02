7fc87527aaab821530ac0754801033ac506955ce
thoniorf <thoniorf@gmail.com>
2016-01-02 17:07:48 +0100

    Updated user verification

--- web/includes/user_verification.php
+++ web/includes/user_verification.php
@@ -13,21 +13,21 @@ $paswd = filter_var($_POST['inputPassword'], FILTER_SANITIZE_STRING);
 // Encrypt passwd
 $paswd = sha1($paswd);
 // Prepare the statemnt
-$stmt = $conn->prepare("SELECT * FROM ticketsys_db.User WHERE Email=? AND Password=?;");
-// Bind vars
-$stmt->bind_param("ss", $email, $paswd);
+// $stmt = $conn->prepare("SELECT * FROM ticketsys_db.User WHERE Email=? AND Password=?;");
+// // Bind vars
+// $stmt->bind_param("ss", $email, $paswd);
 
-$stmt->execute();
-$result = $stmt->get_result();
-$row = $row->fetch_assoc();
-// If row is not null set user_id session
-if($row != null){
-	$_SESSION['user_id'] = $row['idUser'];
-	print "<script>window.location.reload();<script>";
-}
-// If null login failed
-else
-{
-	
-}
+// $stmt->execute();
+// $result = $stmt->get_result();
+// $row = $row->fetch_assoc();
+// // If row is not null set user_id session
+// if($row != null){
+// 	$_SESSION['user_id'] = $row['idUser'];
+// 	print "<script>window.location.reload();<script>";
+// }
+// // If null login failed
+// else
+// {
+		
+// }
 ?>
