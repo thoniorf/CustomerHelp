5f47de3283f4f47188c9c06494ca560d47ce4932
thoniorf <thoniorf@gmail.com>
2016-01-02 16:46:19 +0100

    Updated login.php

--- web/contents/login.php
+++ web/contents/login.php
@@ -3,7 +3,7 @@
 
 
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-8">
         <p class="lead text-center"><h3>Welcome to our Customer Ticket
             System</h3></p>
         <p class="text-justify">Please login below or signup.</p>
